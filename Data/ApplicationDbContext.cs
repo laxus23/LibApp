189e15cf8e260aaf70cba83e4e44a6e2971eb46e
laxus23 <laxus2323@o2.pl>
2022-03-05 17:11:10 +0100

    Dodaj pliki projektów.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LibApp.Models;
+
+namespace LibApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
